b76cef2490555b0eccc87b23f19aff2a8c3dbc98
Chris Schipper <schippcj@miamioh.edu>
2015-11-14 13:51:00 -0500

    Added new Solution

---
+++ PeopleToolbox2/PeopleToolbox2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PeopleToolbox2.Startup))]
+namespace PeopleToolbox2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
