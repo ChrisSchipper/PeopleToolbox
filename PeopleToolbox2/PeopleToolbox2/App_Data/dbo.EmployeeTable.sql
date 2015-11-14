d31778e52207cfd1ce1fe634218c59bb25810d5e
Nate <jonesnf@miamioh.edu>
2015-11-14 15:16:09 -0500

    idk

---
+++ PeopleToolbox2/PeopleToolbox2/App_Data/dbo.EmployeeTable.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Table]
+(
+	[Employee_ID] INT NOT NULL PRIMARY KEY, 
+    [First_Name] VARCHAR(50) NULL, 
+    [Last_Name] VARCHAR(50) NULL, 
+    [ProjectAssigned] VARCHAR(50) NULL, 
+    [Manager_ID] INT NULL, 
+    [Rank_ID] INT NULL, 
+    [Hire_Date] DATE NULL
+)
---
+++ PeopleToolbox2/PeopleToolbox2/App_Data/dbo.Employee_Skills.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Employee_Skills]
+(
+	[Employee_ID] INT NOT NULL, 
+    [Skill_ID] INT NOT NULL, 
+    CONSTRAINT [FK_Employee_Skills_Employee_Table] FOREIGN KEY ([Employee_ID]) REFERENCES [Employee_Table]([Employee_ID]), 
+    CONSTRAINT [FK_Skill_ID_Skills] FOREIGN KEY ([Skill_ID]) REFERENCES [Skills]([Skill_ID])
+)
